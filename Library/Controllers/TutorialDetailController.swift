f0394b1f13d8ad63d053cca495c10de4255f98ed
mauricio <mauricio.sayao@gmail.com>
2020-10-20 14:42:06 -0400

    Adicionado detailview

--- Library/Controllers/TutorialDetailController.swift
+++ Library/Controllers/TutorialDetailController.swift
@@ -8,9 +8,50 @@
 import UIKit
 
 class TutorialDetailController: UIViewController {
+    
+    private let tutorial: Tutorial
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var publishDateLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var queueButton: UIButton!
+    
+    lazy var dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d"
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.title = tutorial.title
+        imageView.image = tutorial.image
+        imageView.backgroundColor = tutorial.imageBackGroundColor
+        titleLabel.text = tutorial.title
+        publishDateLabel.text = tutorial.formattedDate(using: dateFormatter)
+        
+        let buttonTitle = tutorial.isQueued ? "Remove from queue" : "Add to queue"
+        queueButton.setTitle(buttonTitle, for: .normal)
+    }
+    
+    @IBAction func toogleQueued(_ sender: UIButton) {
+        UIView.performWithoutAnimation {
+            if tutorial.isQueued {
+                queueButton.setTitle("Remove from queue", for: .normal)
+            } else {
+                queueButton.setTitle("Add to queue", for: .normal)
+            }
+            
+            self.queueButton.layoutIfNeeded()
+        }
+    }
+    
 }
