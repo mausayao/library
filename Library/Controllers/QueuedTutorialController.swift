a34342fb9fc25da68eff8176ef983d473c939002
mauricio <mauricio.sayao@gmail.com>
2020-10-20 17:32:16 -0400

    queue controller

--- Library/Controllers/QueuedTutorialController.swift
+++ Library/Controllers/QueuedTutorialController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class QueuedTutorialController: UIViewController {
-
+    
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var applyUpdateButton: UIBarButtonItem!
+    @IBOutlet weak var updateButton: UIBarButtonItem!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+        
     }
+    
+    func setupView() {
+        self.title = "Queue"
+        navigationItem.leftBarButtonItem = editButtonItem
+        navigationItem.rightBarButtonItem = nil
+    }
+    @IBAction func deleteSelectedItems(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func applyUpdates(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func triggerUpdates(_ sender: UIBarButtonItem) {
+    }
+}
 
+extension QueuedTutorialController {
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if isEditing {
+            navigationItem.rightBarButtonItems = nil
+            navigationItem.rightBarButtonItem = deleteButton
+        } else {
+            navigationItem.rightBarButtonItem = nil
+            navigationItem.rightBarButtonItems = [self.applyUpdateButton, self.updateButton]
+        }
+        
+        collectionView.allowsMultipleSelection = true
+        collectionView.indexPathsForVisibleItems.forEach { indexPath in
+            guard let cell = collectionView.cellForItem(at: indexPath) as? QueueCell else { return }
+            cell.isEditing = isEditing
+            
+            if !isEditing {
+                cell.isSelected = false
+            }
+        }
+    }
+    
 }
